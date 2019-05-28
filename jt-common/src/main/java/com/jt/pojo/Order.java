d25ae81d8c91a546fa64ce713eebc91e3dba54d7
zkf <798470454@qq.com>
2019-05-28 19:04:37 +0800

    begin

---
+++ jt-common/src/main/java/com/jt/pojo/Order.java
@@ -0,0 +1,41 @@
+package com.jt.pojo;
+
+
+import java.util.Date;
+import java.util.List;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@TableName("tb_order")
+@Data
+@Accessors(chain=true)
+public class Order extends BasePojo{
+	@TableField(exist=false)	//入库操作忽略该字段
+	private OrderShipping orderShipping;
+								//封装订单商品信息  一对多
+	@TableField(exist=false)	//入库操作忽略该字段
+	private List<OrderItem> orderItems;
+	
+	@TableId			
+    private String orderId;		//订单号：登录用户id+当前时间戳',
+    private String payment;
+    private Integer paymentType;
+    private String postFee;
+    private Integer status;		//状态：1、未付款2、已付款3、未发货4、已发货5、交易成功6、交易关闭
+    private Date paymentTime;
+    private Date consignTime;
+    private Date endTime;
+    private Date closeTime;
+    private String shippingName;
+    private String shippingCode;
+    private Long userId;
+    private String buyerMessage;
+    private String buyerNick;
+    private Integer buyerRate;
+
+}
---
+++ jt-common/src/main/java/com/jt/pojo/User.java
@@ -0,0 +1,21 @@
+package com.jt.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@TableName("tb_user")
+@Data
+@Accessors(chain=true)
+public class User extends BasePojo{
+	@TableId(type=IdType.AUTO)	//主键自增
+	private Long id;			//用户id号
+	private String username;	//用户名
+	private String password;	//密码
+	private String phone;		//电话
+	private String email;		//邮箱
+	
+}
